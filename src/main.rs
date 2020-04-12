fddb30a3d5bc117382b5b2ff321204554304d30a
albysmith <48844001+albysmith@users.noreply.github.com>
2020-04-12 11:32:42 -0500

    updtes

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io;
 use std::io::prelude::*;
 use std::io::SeekFrom;
 use std::path::Path;
+use include_dir::{include_dir, Dir};
 #[macro_use]
 extern crate serde;
 
@@ -23,8 +24,6 @@ struct Folder {
 }
 
 fn main() {
-    // Make the tool create separate mods for each dlc folder
-
     // for non-release testing ONLY
     let config: Config = toml::from_str(
         &fs::read_to_string("E:/Rust/Projects/cat_extractor/target/release files/config.toml")
@@ -40,7 +39,11 @@ fn main() {
     // )
     // .expect("toml parsing");
 
-    // for ship mod extraction only
+    const MODS_DIR: Dir = include_dir!("mod_files");
+    for folder in MODS_DIR.dirs() {
+        comp_mod_files(folder, Path::new(&config.output_path));
+    }
+
     let x_rebirth: Folder = Folder {
         outpath: format!("{}/xr_data_dependency", &config.output_path),
         catpath: (&config.input_path).to_string(),
@@ -59,28 +62,6 @@ fn main() {
         mod_id: "xr_data_dependency_homeoflight".to_string(),
         mod_name: "XR Data Dependency - Home of Light".to_string(),
     };
-    // end
-
-    // for total extraction
-    // let x_rebirth: Folder = Folder {
-    //     outpath: format!("{}/xrebirth", &config.output_path),
-    //     catpath: (&config.input_path).to_string(),
-    //     mod_id: "xr_data_dependency".to_string(),
-    //     mod_name: "XR Data Dependency".to_string(),
-    // };
-    // let dlc_home: Folder = Folder {
-    //     outpath: format!("{}/teladioutpost", &config.output_path),
-    //     catpath: format!("{}/extensions/ego_dlc_teladi_outpost", &config.input_path),
-    //     mod_id: "xr_data_dependency_teladioutpost".to_string(),
-    //     mod_name: "XR Data Dependency - Teladi Outpost".to_string(),
-    // };
-    // let dlc_teladi: Folder = Folder {
-    //     outpath: format!("{}/homeoflight", &config.output_path),
-    //     catpath: format!("{}/extensions/ego_dlc_2", &config.input_path),
-    //     mod_id: "xr_data_dependency_homeoflight".to_string(),
-    //     mod_name: "XR Data Dependency - Home of Light".to_string(),
-    // };
-    // end
 
     let folder_vec = vec![x_rebirth, dlc_home, dlc_teladi];
     for folder in folder_vec {
@@ -118,12 +99,12 @@ fn main() {
                         // only copying ship data folders and ANI files
 
                         // for ship mod extraction only
-                        if path.contains("assets/units/")
-                            && (path.contains("data") || path.contains("DATA"))
+                        if (path.contains("assets/units/") && (path.contains("data") || path.contains("DATA"))) ||
+                           (path.contains("EngineSystems") && (path.contains("data") || path.contains("DATA"))) ||
+                           (path.contains("StorageModules") && (path.contains("data") || path.contains("DATA"))) 
+
                         {
                             println!("{}", path);
-                            // end
-
                             // create missing directory folders (will skip automatically if they exist)
                             let filepath =
                                 outpath.join((Path::new(&path)).parent().expect("opening folder"));
@@ -150,14 +131,9 @@ fn main() {
                                 .seek(SeekFrom::Current(bytes_to_read as i64))
                                 .expect("seek");
                         }
-                        // end
-
-                        // break;
                     }
-                    // break;
                 }
             }
-            // for ship mod extraction only
             let content_string = format!("<?xml version=\"1.0\" encoding=\"utf-8\"?>
 <content id=\"{}\" name=\"{}\" description=\"This mod will provide no in-game content without the use of other mods\" author=\"albysmith\" version=\"1\" date=\"2019-10-31\" save=\"0\" enabled=\"1\">
  <text language=\"7\" name=\"{}\" description=\"This mod will provide no in-game content without the use of other mods\" author=\"albysmith\" />
@@ -178,8 +154,24 @@ fn main() {
             content_file
                 .write_all(&content_string.as_bytes())
                 .expect("writing content file");
-            // end
             println!("Extraction Complete");
+        }
+    }
+}
+
+fn comp_mod_files(folder: &Dir, out_path: &Path) {
+    let filepath = Path::new(out_path).join(folder.path());
+    if !filepath.exists() {
+        fs::create_dir_all(filepath).expect("making mod folders");
+    }
+    for file in folder.files() {
+        let path = Path::new(out_path).join(file.path());
+        let mut content_file = File::create(path).expect("creating mod file");
+        content_file.write_all(&file.contents_utf8().expect("reading mod file").as_bytes()).expect("writing mod file");
+    }
+    if folder.dirs().len() >= 1 {
+        for dir in folder.dirs() {
+            comp_mod_files(dir, out_path)
         }
     }
 }
