9d519bce3348dbc882f333c8062461dbdc8909b5
albysmith <48844001+albysmith@users.noreply.github.com>
2020-04-12 21:26:16 -0500

    mod updates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use include_dir::{include_dir, Dir};
 use std::env;
 use std::fs;
 use std::fs::File;
@@ -5,7 +6,6 @@ use std::io;
 use std::io::prelude::*;
 use std::io::SeekFrom;
 use std::path::Path;
-use include_dir::{include_dir, Dir};
 #[macro_use]
 extern crate serde;
 
@@ -13,6 +13,7 @@ extern crate serde;
 struct Config {
     output_path: String,
     input_path: String,
+    extract: bool,
 }
 
 #[derive(Debug, Default)]
@@ -25,116 +26,120 @@ struct Folder {
 
 fn main() {
     // for non-release testing ONLY
+    // let config: Config = toml::from_str(
+    //     &fs::read_to_string("E:/Rust/Projects/cat_extractor/src/config.toml").expect("read config"),
+    // )
+    // .expect("toml parsing");
+
+    // for release version
+    // read config.toml in current directory
     let config: Config = toml::from_str(
-        &fs::read_to_string("E:/Rust/Projects/cat_extractor/target/release files/config.toml")
+        &fs::read_to_string(&env::current_dir().expect("current dir").join("config.toml"))
             .expect("read config"),
     )
     .expect("toml parsing");
 
-    // for release version
-    // read config.toml in current directory
-    // let config: Config = toml::from_str(
-    //     &fs::read_to_string(&env::current_dir().expect("current dir").join("config.toml"))
-    //         .expect("read config")
-    // )
-    // .expect("toml parsing");
-
     const MODS_DIR: Dir = include_dir!("mod_files");
+    let extract_flag = &config.extract;
+
     for folder in MODS_DIR.dirs() {
         comp_mod_files(folder, Path::new(&config.output_path));
     }
 
-    let x_rebirth: Folder = Folder {
-        outpath: format!("{}/xr_data_dependency", &config.output_path),
-        catpath: (&config.input_path).to_string(),
-        mod_id: "xr_data_dependency".to_string(),
-        mod_name: "XR Data Dependency".to_string(),
-    };
-    let dlc_home: Folder = Folder {
-        outpath: format!("{}/xr_data_dependency_teladioutpost", &config.output_path),
-        catpath: format!("{}/extensions/ego_dlc_teladi_outpost", &config.input_path),
-        mod_id: "xr_data_dependency_teladioutpost".to_string(),
-        mod_name: "XR Data Dependency - Teladi Outpost".to_string(),
-    };
-    let dlc_teladi: Folder = Folder {
-        outpath: format!("{}/xr_data_dependency_homeoflight", &config.output_path),
-        catpath: format!("{}/extensions/ego_dlc_2", &config.input_path),
-        mod_id: "xr_data_dependency_homeoflight".to_string(),
-        mod_name: "XR Data Dependency - Home of Light".to_string(),
-    };
+    if extract_flag == &true {
+        let x_rebirth: Folder = Folder {
+            outpath: format!("{}/xr_data_dependency", &config.output_path),
+            catpath: (&config.input_path).to_string(),
+            mod_id: "xr_data_dependency".to_string(),
+            mod_name: "XR Data Dependency".to_string(),
+        };
+        let dlc_home: Folder = Folder {
+            outpath: format!("{}/xr_data_dependency_teladioutpost", &config.output_path),
+            catpath: format!("{}/extensions/ego_dlc_teladi_outpost", &config.input_path),
+            mod_id: "xr_data_dependency_teladioutpost".to_string(),
+            mod_name: "XR Data Dependency - Teladi Outpost".to_string(),
+        };
+        let dlc_teladi: Folder = Folder {
+            outpath: format!("{}/xr_data_dependency_homeoflight", &config.output_path),
+            catpath: format!("{}/extensions/ego_dlc_2", &config.input_path),
+            mod_id: "xr_data_dependency_homeoflight".to_string(),
+            mod_name: "XR Data Dependency - Home of Light".to_string(),
+        };
 
-    let folder_vec = vec![x_rebirth, dlc_home, dlc_teladi];
-    for folder in folder_vec {
-        if Path::new(&folder.catpath).exists() {
-            let outpath = Path::new(&folder.outpath);
-            for entry in fs::read_dir(&folder.catpath).expect("read_dir") {
-                let file = entry.expect("unwrapping entry");
-                let catfile_path = file.path();
-                // only open .cat files that are not sig files
-                if !catfile_path.is_dir()
-                    && catfile_path.extension().expect("unwrapping extension") == "cat"
-                    && !catfile_path
-                        .file_stem()
-                        .expect("unwrapping stem")
-                        .to_str()
-                        .expect("checking for sig")
-                        .contains("sig")
-                {
-                    println!("{:?}", catfile_path);
-                    // find and open matching .dat file
-                    let datfile_path = catfile_path.with_extension("dat");
-                    let mut datfile = &File::open(datfile_path).unwrap();
-                    // read .cat file
-                    let catfile_string =
-                        fs::read_to_string(&catfile_path).expect("unwrapping catfile_string");
-                    for line in catfile_string.lines() {
-                        // split at whitespace from the right edge (reverse) to capture size and other non-path values
-                        let mut iter = line.split_whitespace().rev();
-                        let hash = iter.next().expect("hash");
-                        let modified_epoch = iter.next().expect("epoch");
-                        let size = iter.next().expect("size");
-                        // remove non-path values to get path
-                        let pattern = format!(" {} {} {}", size, modified_epoch, hash);
-                        let path = line.replace(pattern.as_str(), "");
-                        // only copying ship data folders and ANI files
+        let folder_vec = vec![x_rebirth, dlc_home, dlc_teladi];
+        for folder in folder_vec {
+            if Path::new(&folder.catpath).exists() {
+                let outpath = Path::new(&folder.outpath);
+                for entry in fs::read_dir(&folder.catpath).expect("read_dir") {
+                    let file = entry.expect("unwrapping entry");
+                    let catfile_path = file.path();
+                    // only open .cat files that are not sig files
+                    if !catfile_path.is_dir()
+                        && catfile_path.extension().expect("unwrapping extension") == "cat"
+                        && !catfile_path
+                            .file_stem()
+                            .expect("unwrapping stem")
+                            .to_str()
+                            .expect("checking for sig")
+                            .contains("sig")
+                    {
+                        println!("{:?}", catfile_path);
+                        // find and open matching .dat file
+                        let datfile_path = catfile_path.with_extension("dat");
+                        let mut datfile = &File::open(datfile_path).unwrap();
+                        // read .cat file
+                        let catfile_string =
+                            fs::read_to_string(&catfile_path).expect("unwrapping catfile_string");
+                        for line in catfile_string.lines() {
+                            // split at whitespace from the right edge (reverse) to capture size and other non-path values
+                            let mut iter = line.split_whitespace().rev();
+                            let hash = iter.next().expect("hash");
+                            let modified_epoch = iter.next().expect("epoch");
+                            let size = iter.next().expect("size");
+                            // remove non-path values to get path
+                            let pattern = format!(" {} {} {}", size, modified_epoch, hash);
+                            let path = line.replace(pattern.as_str(), "");
+                            // only copying ship data folders and ANI files
 
-                        // for ship mod extraction only
-                        if (path.contains("assets/units/") && (path.contains("data") || path.contains("DATA"))) ||
-                           (path.contains("EngineSystems") && (path.contains("data") || path.contains("DATA"))) ||
-                           (path.contains("StorageModules") && (path.contains("data") || path.contains("DATA"))) 
+                            // for ship mod extraction only
+                            if (path.contains("assets/units/")
+                                && (path.contains("data") || path.contains("DATA")))
+                                || (path.contains("EngineSystems")
+                                    && (path.contains("data") || path.contains("DATA")))
+                                || (path.contains("StorageModules")
+                                    && (path.contains("data") || path.contains("DATA")))
+                            {
+                                println!("{}", path);
+                                // create missing directory folders (will skip automatically if they exist)
+                                let filepath = outpath
+                                    .join((Path::new(&path)).parent().expect("opening folder"));
+                                if !filepath.exists() {
+                                    fs::create_dir_all(filepath).expect("making folders");
+                                }
+                                // read correct amt of bytes from .dat file and copy to writer
+                                let bytes_to_read = size.parse::<u64>().expect("parsing buffer");
+                                let mut buf = vec![];
+                                let mut chunk = datfile.take(bytes_to_read);
+                                chunk.read_to_end(&mut buf).expect("Didn't read enough");
+                                let mut reader: &[u8] = &buf;
+                                let mut writer: Vec<u8> = vec![];
+                                io::copy(&mut reader, &mut writer).expect("shut up");
+                                // write the new files
+                                let out_file_path = outpath.join(&path);
+                                let mut outputfile = File::create(out_file_path).unwrap();
+                                outputfile.write_all(&writer).unwrap();
 
-                        {
-                            println!("{}", path);
-                            // create missing directory folders (will skip automatically if they exist)
-                            let filepath =
-                                outpath.join((Path::new(&path)).parent().expect("opening folder"));
-                            if !filepath.exists() {
-                                fs::create_dir_all(filepath).expect("making folders");
+                            // for ship mod extraction only
+                            } else {
+                                let bytes_to_read = size.parse::<u64>().expect("parsing buffer");
+                                datfile
+                                    .seek(SeekFrom::Current(bytes_to_read as i64))
+                                    .expect("seek");
                             }
-                            // read correct amt of bytes from .dat file and copy to writer
-                            let bytes_to_read = size.parse::<u64>().expect("parsing buffer");
-                            let mut buf = vec![];
-                            let mut chunk = datfile.take(bytes_to_read);
-                            chunk.read_to_end(&mut buf).expect("Didn't read enough");
-                            let mut reader: &[u8] = &buf;
-                            let mut writer: Vec<u8> = vec![];
-                            io::copy(&mut reader, &mut writer).expect("shut up");
-                            // write the new files
-                            let out_file_path = outpath.join(&path);
-                            let mut outputfile = File::create(out_file_path).unwrap();
-                            outputfile.write_all(&writer).unwrap();
-
-                        // for ship mod extraction only
-                        } else {
-                            let bytes_to_read = size.parse::<u64>().expect("parsing buffer");
-                            datfile
-                                .seek(SeekFrom::Current(bytes_to_read as i64))
-                                .expect("seek");
                         }
                     }
                 }
-            }
-            let content_string = format!("<?xml version=\"1.0\" encoding=\"utf-8\"?>
+                let content_string = format!("<?xml version=\"1.0\" encoding=\"utf-8\"?>
 <content id=\"{}\" name=\"{}\" description=\"This mod will provide no in-game content without the use of other mods\" author=\"albysmith\" version=\"1\" date=\"2019-10-31\" save=\"0\" enabled=\"1\">
  <text language=\"7\" name=\"{}\" description=\"This mod will provide no in-game content without the use of other mods\" author=\"albysmith\" />
  <text language=\"33\" name=\"{}\" description=\"This mod will provide no in-game content without the use of other mods\" author=\"albysmith\" />
@@ -149,12 +154,13 @@ fn main() {
  <text language=\"88\" name=\"{}\" description=\"This mod will provide no in-game content without the use of other mods\" author=\"albysmith\" />
 </content>
 ", &folder.mod_id, &folder.mod_name, &folder.mod_name, &folder.mod_name, &folder.mod_name, &folder.mod_name, &folder.mod_name, &folder.mod_name, &folder.mod_name, &folder.mod_name, &folder.mod_name, &folder.mod_name, &folder.mod_name);
-            let mut content_file =
-                File::create(outpath.join("content.xml")).expect("creating content file");
-            content_file
-                .write_all(&content_string.as_bytes())
-                .expect("writing content file");
-            println!("Extraction Complete");
+                let mut content_file =
+                    File::create(outpath.join("content.xml")).expect("creating content file");
+                content_file
+                    .write_all(&content_string.as_bytes())
+                    .expect("writing content file");
+                println!("Extraction Complete");
+            }
         }
     }
 }
@@ -167,7 +173,9 @@ fn comp_mod_files(folder: &Dir, out_path: &Path) {
     for file in folder.files() {
         let path = Path::new(out_path).join(file.path());
         let mut content_file = File::create(path).expect("creating mod file");
-        content_file.write_all(&file.contents_utf8().expect("reading mod file").as_bytes()).expect("writing mod file");
+        content_file
+            .write_all(&file.contents())
+            .expect("writing mod file");
     }
     if folder.dirs().len() >= 1 {
         for dir in folder.dirs() {
